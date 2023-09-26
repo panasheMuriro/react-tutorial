fed0cc1bd8203fe21dc9264429fe01e58aee119c
murirop <panashemuriro2023@u.northwestern.edu>
2023-09-25 21:01:04 -0500

    Style the app

---
+++ src/components/CourseCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const CourseCard = ({ course }) => (
+  <>
+    <div className="card m-1 p-2">
+      <div className="card-body" >
+        <h5 className="card-title">
+          {course.term} CS {course.number}
+        </h5>
+        <p className="card-text">{course.title}</p>
+        <div className="card-footer bg-white">
+          <p className="card-text">{course.meets}</p>
+        </div>
+      </div>
+    </div>
+  </>
+);
